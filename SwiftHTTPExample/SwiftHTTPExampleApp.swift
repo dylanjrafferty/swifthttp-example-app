724c361e974005ebf1a3c0f5b7d81eb5ffd0a41f
dylanjrafferty <34669101+dylanjrafferty@users.noreply.github.com>
2022-09-28 22:32:34 -0400

    Initial Commit

---
+++ SwiftHTTPExample/SwiftHTTPExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftHTTPExampleApp.swift
+//  SwiftHTTPExample
+//
+//  Created by Dylan Rafferty on 9/28/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftHTTPExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
